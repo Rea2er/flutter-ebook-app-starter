7c442d71b3e3ec33a501855d295cf5f2b39b853d
Ruize Nie <rz376775@dal.ca>
2021-10-13 20:15:53 -0300

    project modify

---
+++ android/app/src/main/kotlin/com/example/ebook_app_starter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ebook_app_starter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/colors.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const Color kFont = Color(0xFF180141);
